632ad55d98636b301de9d750279bca49a9e74c40
Kembrige <49879155+Kembrige@users.noreply.github.com>
2024-01-06 04:20:43 +0500

    Add files via upload

---
+++ AS7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());
+
+        string binary = "";
+
+        while (num > 0)
+        {
+            int remainder = num % 2;
+            binary = remainder + binary;
+            num /= 2;
+        }
+
+        Console.WriteLine("Двоичная запись: " + binary);
+    }
+}
